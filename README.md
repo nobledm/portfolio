c5f760c35ecac95a94662f50a06bf6a2ca5ecc1f
nobledm <40868595+nobledm@users.noreply.github.com>
2020-03-03 08:47:21 -0700

    cleaned out old site, replaced with gulp pipeline

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-
 # PORTFOLIO
 
-TODO: substance
+Revamping the whole thing. Complete WiP. Aiming to be a quick one-page summary with some links to git projects'n whatnot. Will hopefully jazz it up later when not time crunched.
---
+++ gulpfile.js
@@ -0,0 +1,94 @@
+const gulp = require("gulp"),
+  devBuild = process.env.NODE_ENV !== "production",
+  src = "src/",
+  build = "build/",
+  noop = require("gulp-noop"),
+  newer = require("gulp-newer"),
+  imagemin = require("gulp-imagemin"),
+  htmlclean = require("gulp-htmlclean"),
+  concat = require("gulp-concat"),
+  deporder = require("gulp-deporder"),
+  terser = require("gulp-terser"),
+  stripdebug = devBuild ? null : require("gulp-strip-debug"),
+  sourcemaps = devBuild ? require("gulp-sourcemaps") : null,
+  sass = require("gulp-sass"),
+  postcss = require("gulp-postcss"),
+  assets = require("postcss-assets"),
+  autoprefixer = require("autoprefixer"),
+  mqpacker = require("css-mqpacker"),
+  cssnano = require("cssnano");
+
+function images() {
+  const out = build + "images/";
+
+  return gulp
+    .src(src + "images/**/*")
+    .pipe(newer(out))
+    .pipe(imagemin({ optimizationLevel: 5 }))
+    .pipe(gulp.dest(out));
+}
+
+function html() {
+  const out = build + "pages/";
+
+  return gulp
+    .src(src + "pages/**/*")
+    .pipe(newer(out))
+    .pipe(devBuild ? noop() : htmlclean())
+    .pipe(gulp.dest(out));
+}
+
+function js() {
+  return gulp
+    .src(src + "js/**/*")
+    .pipe(sourcemaps ? sourcemaps.init() : noop())
+    .pipe(deporder())
+    .pipe(concat("main.js"))
+    .pipe(stripdebug ? stripdebug() : noop())
+    .pipe(terser())
+    .pipe(sourcemaps ? sourcemaps.write() : noop())
+    .pipe(gulp.dest(build + "js/"));
+}
+
+function css() {
+  return gulp
+    .src(src + "scss/style.scss")
+    .pipe(sourcemaps ? sourcemaps.init() : noop())
+    .pipe(
+      sass({
+        outputStyle: "nested",
+        imagePath: "/images/",
+        precision: 3,
+        errLogToConsole: true
+      }).on("error", sass.logError)
+    )
+    .pipe(
+      postcss([
+        assets({ loadPaths: ["images/"] }),
+        autoprefixer({ browsers: ["last 2 versions", "> 2%"] }),
+        mqpacker,
+        cssnano
+      ])
+    )
+    .pipe(sourcemaps ? sourcemaps.write() : noop())
+    .pipe(gulp.dest(build + "css/"));
+}
+
+exports.html = gulp.series(images, html);
+exports.js = js;
+exports.css = gulp.series(images, css);
+
+exports.build = gulp.parallel(exports.html, exports.js, exports.css);
+
+function watch(done) {
+  gulp.watch(src + "images/**/*", images);
+  gulp.watch(src + "pages/**/*", html);
+  gulp.watch(src + "js/**/*", js);
+  gulp.watch(src + "scss/**/*", css);
+
+  done();
+}
+
+exports.watch = watch;
+
+exports.default = gulp.series(exports.build, exports.watch);
